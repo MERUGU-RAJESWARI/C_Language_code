c20949dc07387e506dcb24feff8b14db28c41adf
MERUGU-RAJESWARI <139354752+MERUGU-RAJESWARI@users.noreply.github.com>
2025-03-13 05:18:32 +0000

    happy

--- AddTwoNumbersTest.java
+++ AddTwoNumbersTest.java
@@ -7,8 +7,5 @@ public class AddTwoNumbersTest {
     @Test
     void testAddition() {
         assertEquals(30, obj.add(10, 20), "10 + 20 should be 30");
-        assertEquals(0, obj.add(0, 0), "0 + 0 should be 0");
-        assertEquals(-10, obj.add(-5, -5), "-5 + (-5) should be -10");
-        assertEquals(5, obj.add(10, -5), "10 + (-5) should be 5");
     }
 }
